6c9820d436d46c1c1b5d8601f6c60225d3d8ac9d
mhamed <mohamedhame2ed@gmail.com>
2023-01-09 17:04:44 +0200

    add realod button

--- src/App.tsx
+++ src/App.tsx
@@ -79,6 +79,9 @@ function App() {
         {isLoser && "Nice Try - Refresh to try again"}
       </div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      {isWinner || isLoser ?<button className="bg-red-300 rounded-lg p-2 text-white" onClick={()=> window.location.reload()}>try again  </button> : ''}
+      <center className="text-[30px] uppercase" > first liter is : <span className="text-red-500">{wordToGuess.slice(0,1)} </span> and last liter  :   <span className="text-red-500">{wordToGuess.slice(-1)}</span></center>
+
       <HangmanWord
         reveal={isLoser}
         guessedLetters={guessedLetters}
